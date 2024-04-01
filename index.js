5581a18c48e430f59b1ad46973f9ee1046fb561f
Peter Ndungu <petersonndungu995@gmail.com>
2024-04-01 15:29:53 +0300

    “completed”

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(numerator, denominator) {
+    if (denominator === 0) {
+        return "Error: Division by zero is not allowed.";
+    }
+    return numerator / denominator;
+};
+
+console.log(divide(2000, 100)); 
+const square=number=>{
+    return number*number
+}
+const add=(parameter1,parameter2)=>{
+    return parameter1+parameter2
+}
+
